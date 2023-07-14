bf4ffad72619d08ef022b009eaa039ef3bc0d94e
robinNcode <msmrobin518@gmail.com>
2023-07-14 10:47:55 +0600

    `refactor`: arguments name changed.

--- src/Translate.php
+++ src/Translate.php
@@ -14,27 +14,39 @@ class EnglishToBanglaDate{
     private $bangOporannho = array('পূর্বাহ্ন', 'অপরাহ্ন');
     private $bangMonth = array('জানুয়ারি', 'ফেব্রুয়ারি', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'আগস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর');
 
-    public function banglaDigit($english_date) {
-        return str_replace($this->engNumber, $this->bangNumber, $english_date);
+    public function banglaDigit($english_digit) {
+        return str_replace($this->engNumber, $this->bangNumber, $english_digit);
     }
 
-    public function banglaMonth($english_date){
-        return str_replace($this->engMonth, $this->bangMonth, $english_date);
+    public function banglaMonth($english_month){
+        return str_replace($this->engMonth, $this->bangMonth, $english_month);
     }
 
-    public function banglaDay($english_date){
-        return str_replace($this->engDay, $this->bangDay, $english_date);
+    public function banglaDay($english_day){
+        return str_replace($this->engDay, $this->bangDay, $english_day);
     }
 
-    public function banglaOporannho($english_date){
-        return str_replace($this->engOporannho, $this->bangOporannho, $english_date);
+    public function banglaOporannho($english_oporannho){
+        return str_replace($this->engOporannho, $this->bangOporannho, $english_oporannho);
     }
 
-    public function banglaNth($english_date){
-        return str_replace($this->engNth, $this->bangNth, $english_date);
+    public function banglaNth($english_nth){
+        return str_replace($this->engNth, $this->bangNth, $english_nth);
     }
 
     public function banglaDate($english_date){
+        $english_date = date('jS F, Y', strtotime($english_date));
+
+        $bangla_date = $this->banglaDigit($english_date);
+        $bangla_date = $this->banglaNth($bangla_date);
+        $bangla_date = $this->banglaDay($bangla_date);
+        $bangla_date = $this->banglaOporannho($bangla_date);
+        $bangla_date = $this->banglaMonth($bangla_date);
+
+        return $bangla_date;
+    }
+
+    public function banglaDateTime($english_date){
         $english_date = date('h:i a jS F, Y', strtotime($english_date));
 
         $bangla_date = $this->banglaDigit($english_date);
